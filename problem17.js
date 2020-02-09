37c042f55b51ad167510f1aab3c6bfca37c92bba
Rangarajan Sivaraman <rangarajanps@gmail.com>
2020-02-09 19:28:40 +0000

    Update problem17.js

--- problem17.js
+++ problem17.js
@@ -1,75 +1,44 @@
+function convert(n) {
+
+  var units = [ "", "One", "Two", "Three", "Four",
+"Five", "Six", "Seven", "Eight", "Nine", "Ten", "Eleven", "Twelve",
+"Thirteen", "Fourteen", "Fifteen", "Sixteen", "Seventeen",
+"Eighteen", "Nineteen" ];
+
+var tens = ["", "",	"Twenty", "Thirty", "Forty", "Fifty", "Sixty", "Seventy", "Eighty", 	"Ninety"];
+
+	if (n < 20) {
+		return units[n];
+	}
+
+	if (n < 100) {
+		return tens[parseInt(n / 10)]+ " " + units[n % 10];
+	}
+
+	if (n < 1000 ) {
+
+    if ( n % 100 == 0 ) {
+      return units[parseInt(n / 100)] + " Hundred";
+    } else {
+		  return units[parseInt(n / 100)] + " Hundred" + " and " + convert(n % 100);
+    }
+	}
+
+	if (n < 100000) {
+		return convert(n / 1000) + " Thousand" + " " +  convert(n % 1000);
+	}
+
+	if (n < 10000000) {
+		return convert(n / 100000) + " Lakh" + " "  + convert(n % 100000);
+	}
+
+	return convert(n / 10000000) + " Crore" + " "  + convert(n % 10000000);
+}
+
 function convertNumberToWord(number) {
 
-  var oneth = ["","One","Two","Three", "Four", "Five", "Six", "Seven", "Eight", "Nine", "Ten", "Eleven","Twelve", "Thirteen","Fourteen","Fifteen", "Sixteen", "Seventeen", "Eighteen", "Nineteen", "Twenty"];
-  var tenth = ["","","Twenty","Thirty","Forty","Fifty","Sixty","Seventy","Eighty","Ninety"];
-  var others = ["","","Hundred","","Thousand"];
-  
-  if ( number <= 19) {
-    //console.log("Number to word is "+oneth[number]);
-    return oneth[number].length;
-  }
-
-  var len = 0;
-  var original = number;
-  var digits = [];
-  while (number >=1) {
-    digits.push(number%10);
-    number = parseInt(number / 10);
-    len++;
-  }
-  //digits = digits.reverse();
-  //console.log("Number of digits in array is "+len+" "+digits);
-
-  var words;
-  if ( original > 19 && original < 100 ) {
-    words = tenth[digits[1]]+" "+oneth[digits[0]];
-  }
-
-  var digit = 0, remind = 0;
-  if (original == 100) {
-    words = "One hundred";
-  }
-  if ( original > 100 && original < 1000 ) {
-    digit = parseInt(original / 100);
-    remind = original % 100;
-    if ( remind > 19) {
-      words = oneth[digit] +" hundred and "+tenth[digits[1]]+" "+oneth[digits[0]];
-    } else {
-      words = oneth[digit] +" hundred and "+oneth[remind];
-    }
-    
-  }
-
-  if (original == 1000) {
-    words = "One Thousand";
-  }
-  if ( original > 1000 && original < 10000 ) {
-    digit = parseInt(original / 1000);
-    remind = original % 1000;
-    if ( remind > 19) {
-      words = oneth[digit] +" Thousand "+oneth[digits[2]] +" hundred and "+tenth[digits[1]]+" "+oneth[digits[0]];
-    } else {
-      words = oneth[digit] +" Thousand "+oneth[digits[2]] +" hundred and "+oneth[remind];
-    }
-    console.log("Number in words for "+original+" is "+words);
-  }
-  //console.log("Number in words for "+original+" is "+words);
-  
-/*
-  
-  var digit = 0;
-  for (var i = 0; i < len; i++) {
-    digit = original % Math.pow(10, i+1);
-    //original = original / 10;
-    if ( i == 0) {
-      words[i] = oneth[digit];
-    } else if ( i == 1) {
-      words[i] = tenth[digit];
-    }
-  }
-  
-*/
-
+  var words = convert(number);
+  //console.log("Number in words for "+number+" is "+words);
   var ans = 0;
   for (var i = 0; i < words.length; i++) {
     words = words.replace(/\s/g,"");
@@ -89,4 +58,4 @@ function numberLetterCounts(limit) {
   return ans;
 }
 
-console.log(numberLetterCounts(1000));
+numberLetterCounts(150);
