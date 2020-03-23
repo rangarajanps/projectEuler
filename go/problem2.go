2fb716190b5addbc3f919e17555b0446418126d5
Rangarajan Sivaraman <rangarajanps@gmail.com>
2020-03-23 13:04:02 +0000

    Even Fibonacci Numbers

---
+++ go/problem2.go
@@ -0,0 +1,36 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+
+	fmt.Println(SumOfEvenFibonacciNumber(10))
+}
+
+func SumOfEvenFibonacciNumber(limit int) int {
+
+	var fibSeries = make([]int, limit)
+	fibSeries[0] = 1
+	fibSeries[1] = 2
+	counter := 2
+
+	for i := counter; i < limit; i++ {
+		fibSeries[counter] = fibSeries[counter-1] + fibSeries[counter-2]
+		counter++
+	}
+	//fmt.Println("Value of Fibonacci Sereis is ", fibSeries)
+
+	sum := 0
+	for j := 0; j < len(fibSeries); j++ {
+		if fibSeries[j] > limit {
+			break
+		}
+		if fibSeries[j]%2 == 0 {
+			sum = sum + fibSeries[j]
+		}
+	}
+	//fmt.Println("Sum of Even numbers is ", sum)
+	return sum
+}
