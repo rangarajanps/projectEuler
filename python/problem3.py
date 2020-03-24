7efe504b01edaa4d175c46165000e07983950999
Rangarajan Sivaraman <rangarajanps@gmail.com>
2020-03-24 12:37:44 +0000

    Largest prime factor

---
+++ python/problem3.py
@@ -0,0 +1,33 @@
+def sieve(n):
+    primes = []
+    primeNumbers = []
+
+    for x in range(1,n+1):
+        primes.append(True)
+
+    for x in range(2,n//2):
+        y=2
+        while x*y<n:
+            primes[x*y]=False
+            y +=1
+
+    for x in range(1, n):
+        if primes[x]:
+            primeNumbers.append(x)
+
+    return primeNumbers
+
+def largestPrimeFactor(n):
+    primeList = sieve(int(n ** (1 / 2))+1)
+    if isPrime(n): return n
+
+    largestFactor = 1
+    for x in primeList:
+        if n/x < 0: break
+        if n % x == 0 and x > largestFactor:
+            largestFactor = x
+
+    return largestFactor
+
+if __name__ == '__main__':
+    print(largestPrimeFactor(13195))
