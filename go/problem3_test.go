8d4b66a085b12bd2370f01244aaafa4f41a09919
Rangarajan Sivaraman <rangarajanps@gmail.com>
2020-03-24 14:44:10 +0000

    Test case for Problem3

---
+++ go/problem3_test.go
@@ -0,0 +1,27 @@
+package main
+
+import (
+	"testing"
+)
+
+func TestLargestPrimeFactor(t *testing.T) {
+
+	tables := []struct {
+		input    int
+		expected int
+	}{
+		{2, 2},
+		{3, 3},
+		{5, 5},
+		{7, 7},
+		{13195, 29},
+		{600851475143, 6857},
+	}
+
+	for _, table := range tables {
+		total := LargestPrimeFactor(table.input)
+		if total != table.expected {
+			t.Errorf("TestSumOfEvenFibonacciNumber of (%d) was incorrect, got: %d, want: %d.", table.input, total, table.expected)
+		}
+	}
+}
