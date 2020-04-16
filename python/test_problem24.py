69fb6f99e10698afd7abeb34a5a461e64daf0a4b
Rangarajan Sivaraman <rangarajanps@gmail.com>
2020-04-16 20:00:25 +0100

    Test case for Problem24

--- python/test_problem24.py
+++ python/test_problem24.py
@@ -5,9 +5,9 @@ class MyTestcase(unittest.TestCase):
     def test_lexicographicPermutation(self):
         testData = {
             699999: 1938246570,
-		    899999: 2536987410,
-		    900000: 2537014689,
-		    999999: 2783915460,
+	    899999: 2536987410,
+	    900000: 2537014689,
+	    999999: 2783915460,
 	    }
         for key in testData:
             self.assertEqual(testData[key], lexicographicPermutation(key))
